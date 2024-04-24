971204f5c3f35bdffa7aab1f5f3e7132fe99fb16
HHAvans <hj.hoogland@student.avans.nl>
2024-04-24 10:16:23 +0200

    update

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const app = express()
 
 const port = process.env.PORT || 3000
 
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+})
+
 app.all('*', (req, res, next) => {
     console.log('Request:', req.method, req.url)
     next()
@@ -14,7 +18,6 @@ app.get('/', function (req, res) {
 })
 
 app.get('/api/info', (req, res) => {
-    console.log('GET /api/info')
     const info = {
         name: 'My Nodejs Express server',
         version: '0.0.1',
@@ -24,7 +27,6 @@ app.get('/api/info', (req, res) => {
 })
 
 app.get('/api/users', (req, res) => {
-    // console.log('GET /api/users')
     dtb.getAll((err, data) => {
         if (err) {
             res.status(500).json(err)
@@ -32,8 +34,4 @@ app.get('/api/users', (req, res) => {
             res.status(200).json(data)
         }
     })
-})
-
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
 })

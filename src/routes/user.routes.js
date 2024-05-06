e66d2a1fbdc6fa348ada25153e2129aba4d994ff
mggluijten <mgg.luijten@student.avans.nl>
2024-05-06 19:15:24 +0200

    Update user.routes.js

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -73,7 +73,7 @@ const validateUserCreateChaiExpect = (req, res, next) => {
 }
 
 // Userroutes
-router.post('/api/users', userController.create)
+router.post('/api/users', userController.add)
 router.get('/api/users', userController.getAll)
 router.get('/api/users/:userId', userController.getById)
 

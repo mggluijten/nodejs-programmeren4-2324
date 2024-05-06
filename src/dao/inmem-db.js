a6068479aea684e82568f03056c1d56f61e46e29
mggluijten <mgg.luijten@student.avans.nl>
2024-05-06 20:09:55 +0200

    naar main

--- src/dao/inmem-db.js
+++ src/dao/inmem-db.js
@@ -1,80 +1,86 @@
-//
-// Onze lokale 'in memory database'.
-// We simuleren een asynchrone database met een array van objecten.
-// De array bevat een aantal dummy records.
-// De database heeft twee methoden: get en add.
-// Opdracht: Voeg de overige methoden toe.
-//
 const database = {
-    // het array met dummy records. Dit is de 'database'.
     _data: [
         {
             id: 0,
             firstName: 'Hendrik',
             lastName: 'van Dam',
-            emailAdress: 'hvd@server.nl'
-            // Hier de overige velden uit het functioneel ontwerp
+            street: 'Example Street 1',
+            city: 'Example City',
+            isActive: true,
+            emailAddress: 'hvd@server.nl',
+            password: 'password123',
+            phoneNumber: '1234567890'
         },
         {
             id: 1,
             firstName: 'Marieke',
             lastName: 'Jansen',
-            emailAdress: 'm@server.nl'
-            // Hier de overige velden uit het functioneel ontwerp
+            street: 'Example Street 2',
+            city: 'Example City',
+            isActive: false,
+            emailAddress: 'm@server.nl',
+            password: 'password123',
+            phoneNumber: '0987654321'
         }
     ],
-
-    // Ieder nieuw item in db krijgt 'autoincrement' index.
-    // Je moet die wel zelf toevoegen aan ieder nieuw item.
     _index: 2,
     _delayTime: 500,
 
     getAll(callback) {
-        // Simuleer een asynchrone operatie
         setTimeout(() => {
-            // Roep de callback aan, en retourneer de data
-            callback(null, this._data)
-        }, this._delayTime)
+            console.log('Database Data:', this._data);  // Logging to check data integrity
+            callback(null, this._data);
+        }, this._delayTime);
     },
+    
 
     getById(id, callback) {
-        // Simuleer een asynchrone operatie
         setTimeout(() => {
-            if (id < 0 || id >= this._data.length) {
-                callback({ message: `Error: id ${id} does not exist!` }, null)
+            const user = this._data.find(u => u.id === id);
+            if (!user) {
+                callback({ message: `Error: id ${id} does not exist!` }, null);
             } else {
-                callback(null, this._data[id])
+                callback(null, user);
             }
-        }, this._delayTime)
+        }, this._delayTime);
     },
 
     add(item, callback) {
-        // Simuleer een asynchrone operatie
         setTimeout(() => {
-            // Voeg een id toe en voeg het item toe aan de database
-            item.id = this._index++
-            // Voeg item toe aan de array
-            this._data.push(item)
-
-            // Roep de callback aan het einde van de operatie
-            // met het toegevoegde item als argument, of null als er een fout is opgetreden
-            callback(null, item)
-        }, this._delayTime)
-    }
-
-    // Voeg zelf de overige database functionaliteit toe
-    deleteById(id, callback) {
-        // Simuleer een asynchrone operatie
-        setTimeout(() => {
-            if (id < 0 || id >= this._data.length) {
-                callback({ message: `Error: id ${id} does not exist!` }, null)
+            const exists = this._data.some(u => u.emailAddress === item.emailAddress);
+            if (exists) {
+                callback({ message: "Error: Email address already exists!" }, null);
             } else {
-                this._data.splice(id, 1)
-                callback(null, {message: "Item deleted."})
+                item.id = this._index++;
+                this._data.push(item);
+                callback(null, item);
             }
-        }, 1500)
-    }
-}
+        }, this._delayTime);
+    },
 
-module.exports = database
-// module.exports = database.index;
+    updateById(id, newData, callback) {
+        setTimeout(() => {
+            const index = this._data.findIndex(user => user.id === id);
+            if (index === -1) {
+                callback({ message: `Error: User with id ${id} not found!` }, null);
+            } else {
+                this._data[index] = { ...this._data[index], ...newData };
+                callback(null, this._data[index]);
+            }
+        }, this._delayTime);
+    },
+
+    deleteById(id, callback) {
+        setTimeout(() => {
+            const index = this._data.findIndex(user => user.id === id);
+            if (index === -1) {
+                callback({ message: `Error: User with id ${id} not found!` }, null);
+            } else {
+                this._data.splice(index, 1);
+                callback(null, { message: `User with id ${id} deleted successfully.` });
+            }
+        }, this._delayTime);
+    }
+};
+
+module.exports = database;

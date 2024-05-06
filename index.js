d0f281c2ee7d420cd2371659c656f0238d9ce510
mggluijten <mgg.luijten@student.avans.nl>
2024-05-06 20:26:01 +0200

    listener twee keer

--- index.js
+++ index.js
@@ -1,38 +1,34 @@
-const express = require('express')
-const userRoutes = require('./src/routes/user.routes')
+const express = require('express');
+const userRoutes = require('./src/routes/user.routes');
+const logger = require('tracer').console();
 
-const app = express()
-const logger = require('tracer').console()
+const app = express();
 
 // express.json zorgt dat we de body van een request kunnen lezen
-app.use(express.json())
+app.use(express.json());
 
-const port = process.env.PORT || 3000
-
-app.listen(port, () => {
-    logger.debug(`Running on part ${port}.`)
-})
+const port = process.env.PORT || 3000;
 
 app.all('*', (req, res, next) => {
-    logger.info(`REQUEST: ${req.method} ${req.url}`)
-    next()
-})
+    logger.info(`REQUEST: ${req.method} ${req.url}`);
+    next();
+});
 
 app.get('/', function (req, res) {
-    res.json({ message: 'Hello World' })
-})
+    res.json({ message: 'Hello World' });
+});
 
 app.get('/api/info', (req, res) => {
     const info = {
         name: 'My Nodejs Express server',
         version: '0.0.1',
         description: 'This is a simple Nodejs Express server'
-    }
-    res.json(info)
-})
+    };
+    res.json(info);
+});
 
 // Hier komen alle routes
-app.use(userRoutes)
+app.use(userRoutes);
 
 // Hier komt de route error handler te staan!
 app.use((req, res, next) => {
@@ -40,8 +36,8 @@ app.use((req, res, next) => {
         status: 404,
         message: 'Route not found',
         data: {}
-    })
-})
+    });
+});
 
 // Hier komt je Express error handler te staan!
 app.use((error, req, res, next) => {
@@ -49,12 +45,10 @@ app.use((error, req, res, next) => {
         status: error.status || 500,
         message: error.message || 'Internal Server Error',
         data: {}
-    })
-})
+    });
+});
 
+// Only one app.listen() should be present
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
-})
-
-// Deze export is nodig zodat Chai de server kan opstarten
-module.exports = app
+    console.log(`Server is running on port ${port}`);
+});
